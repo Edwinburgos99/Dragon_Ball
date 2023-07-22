6e90fee906f573869646b8952f9dd3223e18df04
Edwinburgos99 <edwin.burgos@correounivalle.edu.co>
2023-07-22 14:44:08 -0500

    Add: se agrego y se modifico algunos archivos de la carpeta Personajes, se añadio una nueva imagen en la carpeta Img

---
+++ Personajes/Personajes.js
@@ -0,0 +1,10 @@
+function showTooltip() {
+  const tooltip = document.getElementById("tooltip");
+  tooltip.style.display = "block";
+  console.log("Tooltip mostrado");
+}
+
+function hideTooltip() {
+  const tooltip = document.getElementById("tooltip");
+  tooltip.style.display = "none";
+}
