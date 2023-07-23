0b7b7d653248cede8ae0a0e13b846efe9e8b94ab
Edwinburgos99 <edwin.burgos@correounivalle.edu.co>
2023-07-23 00:29:38 -0500

    Add: se agregaron imagenes a la carpeta Img, se modifico los archivos de la carpeta Personajes, y el html y css de la carpeta Main

--- Personajes/Personajes.js
+++ Personajes/Personajes.js
@@ -1,10 +1,39 @@
 function showTooltip() {
   const tooltip = document.getElementById("tooltip");
   tooltip.style.display = "block";
-  console.log("Tooltip mostrado");
 }
 
 function hideTooltip() {
   const tooltip = document.getElementById("tooltip");
+  tooltip.style.display = "none";
+}
+
+function showTooltip1() {
+  const tooltip = document.getElementById("tooltip1");
+  tooltip.style.display = "block";
+}
+
+function hideTooltip1() {
+  const tooltip = document.getElementById("tooltip1");
+  tooltip.style.display = "none";
+}
+
+function showTooltip2() {
+  const tooltip = document.getElementById("tooltip2");
+  tooltip.style.display = "block";
+}
+
+function hideTooltip2() {
+  const tooltip = document.getElementById("tooltip2");
+  tooltip.style.display = "none";
+}
+
+function showTooltip3() {
+  const tooltip = document.getElementById("tooltip3");
+  tooltip.style.display = "block";
+}
+
+function hideTooltip3() {
+  const tooltip = document.getElementById("tooltip3");
   tooltip.style.display = "none";
 }
